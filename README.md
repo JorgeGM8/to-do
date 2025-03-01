89f4491b099597c36b6e77b3b2a192801524a359
JorgeGM8 <jorgegaleanom+github@gmail.com>
2025-03-01 18:50:00 +0100

    Create task, view list, other edits

--- README.md
+++ README.md
@@ -1,17 +1,27 @@
 # Description
 Simple to-do list program. *UNDER CONSTRUCTION*
 
+# Instructions
+You will need to have Python >=3.1.0 installed and just one lightweight library, called "tabulate".
+
+You may install the dependency directly or using the requirements.txt file.
+Examples:
+    With pip:
+        `pip install -r requirements.txt`
+    With conda:
+        `conda install --file requirements.txt`
+    With uv:
+        `uv add -r requirements.txt`
+
 # Completed
 ✅ Create task, date and description.
 
-✅ Display task list (task and date).
+✅ Display task list (number, task, date and description).
 
 # Pending
 ⚡ Modify task.
 
 ⚡ Delete task.
-
-⚡ View task description.
 
 ⚡ Error handling.
 
--- To-do.py
+++ To-do.py
@@ -1,6 +1,8 @@
 import gettext
 import sqlite3
+from tabulate import tabulate
 
+# Language configuration.
 def select_language(language):
 	localedir = 'locales'
 	translation = gettext.translation('messages', localedir, languages=[language], fallback=True)
@@ -11,15 +13,19 @@ def select_language(language):
 language = input('Choose language/elige idioma (en/es): ').strip()
 select_language(language)
 
+
+# Database and table creation.
 con = sqlite3.connect('tasks.db')
 cur = con.cursor()
 
 try:
-	cur.execute(f'CREATE TABLE tasks({tr('TASK, DUE DATE, DESCRIPTION')})')
+	cur.execute(tr('CREATE TABLE tasks(TASK VARCHAR(100) NOT NULL, "DUE DATE" DATE, DESCRIPTION VARCHAR(300))'))
+	con.commit()
 except:
-	pass # Check if this works properly - should create table if doesn't exist, else pass.
+	pass # (If table does exist, pass)
 
 
+# Program itself.
 while True:
 	print(tr('\n--------------------\n\t\tTO-DO\n\nPlease, choose the action you want to do:\n'
 	   '1 -> Create a new task.\n'
@@ -28,34 +34,47 @@ while True:
 	
 	match int(input()):
 		case 1: # Create a new task.
-			pass
-		
+			name = input(tr('Name of new task: '))
+			date = input(tr('Due date in DD/MM/YYYY format (optional): '))
+			description = input(tr('Description (optional): '))
+			data = [name, date, description]
+
+			cur.execute('''INSERT INTO tasks
+			   VALUES (?, ?, ?)
+			''', data)
+			con.commit()
+
 		case 2: # View task list.
-			pass
-			
-			match int(input(tr('\n--------------------\nWhat do you want to do now?:\
-				   1 -> Modify a task.\
-				   2 -> Delete a task.\
-				   3 -> View task description.\
-				   0 -> Go to main menu.\n--------------------'))):
-				case 1: # Modify a task.
-					pass
+			rows = cur.execute('SELECT * FROM tasks').fetchall()
+			headers = [tr('No.')] + [desc[0] for desc in cur.description]
+			rows = [(i+1, *row) for i, row in enumerate(rows)]
 
-				case 2: # Delete a task.
-					pass
-
-				case 3: # View task description.
-					pass
-
-				case 0: # Go to main menu.
-					continue
-
-				case _:
-					pass # Add method to report error and come back to match.
+			print(tabulate(rows, headers=headers, tablefmt='grid'))
 					
 		case 0: # Close program.
+			con.close()
 			print(input(tr('End of program. Clic Enter to exit.')))
 			break
+
+		case _:
+			pass # Add method to report error and repeat match.
+
+	match int(input(tr('\n--------------------\nWhat do you want to do now?:\n'
+			'1 -> Modify a task.\n'
+			'2 -> Delete a task.\n'
+			'3 -> View task description.\n'
+			'0 -> Go to main menu.\n--------------------'))):
+		case 1: # Modify a task.
+			pass
+
+		case 2: # Delete a task.
+			pass
+
+		case 3: # View task description.
+			pass
+
+		case 0: # Go to main menu.
+			continue
 
 		case _:
 			pass # Add method to report error and come back to match.
